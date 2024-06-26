ebb836e1ee327a1185f4167da4b0f5f92434c90b
hengelcen <hzelaya@harding.edu>
2024-06-26 16:41:49 -0600

    final changes

--- code/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('showInfo').addEventListener('click', function() {
-    document.querySelector('.card').classList.add('show-info');
-});
-
-document.getElementById('hideInfo').addEventListener('click', function() {
-    document.querySelector('.card').classList.remove('show-info');
-});
---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleCard() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('show-info');
+}
